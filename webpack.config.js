0d802ededc48220732ea97f8d444265d7b130f64
Marcelo Rebouças <marcelosreboucas@gmail.com>
2019-12-03 08:26:42 -0300

    Babel configuration

--- webpack.config.js
+++ webpack.config.js
@@ -8,5 +8,17 @@ module.exports = {
         path : path.join(__dirname, 'dist'),
         filename: 'bundle.js',
         publicPath: '/static/'
+    },
+    /**
+     * Configuração do BABEL.
+     * Colquei as entradas no .babelrc
+     */
+    module: {
+        loaders:[{
+            teste: /\.js$/,
+            exclude: /node_modules/,
+            include: /src/,
+            loader: 'babel'
+        }]
     }
 }
